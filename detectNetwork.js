aae556f630b4c01dd522e7b4ea45b23e73d0779f
Alex Romero <alexromeroreyes09@gmail.com>
2021-10-24 18:31:47 -0700

    Assignment Completed.

--- detectNetwork.js
+++ detectNetwork.js
@@ -15,7 +15,11 @@ var detectNetwork = function(cardNumber) {
   // Once you've read this, go ahead and try to implement this function, then return to the console.
 
   //User Card Number Info
-  var cnPrefix = parseInt(cardNumber.slice(0, 2));
+  var cnOnePrefix = cardNumber.slice(0, 1);
+  var cnTwoPrefix = cardNumber.slice(0, 2);
+  var cnThreePrefix = cardNumber.slice(0, 3);
+  var cnFourPrefix = cardNumber.slice(0, 4);
+  var cnSixPrefix = cardNumber.slice(0, 6);
   var cnLength = cardNumber.length;
   //Diner's Club Info
   var dcPrefix = [38, 39];
@@ -23,9 +27,61 @@ var detectNetwork = function(cardNumber) {
   //American Express Info
   var axPrefix = [34, 37];
   var axLength = 15;
-  if (cnPrefix === dcPrefix[0] || cnPrefix === dcPrefix[1] && cnLength === dcLength ) {
+  //Visa Info
+  var visaPrefix = 4
+  var visaLength = [13, 16, 19];
+  //MasterCard Info
+  var mcPrefix = [51, 52, 53, 54, 55];
+  var mcLength = 16;
+  //Discover Info
+  var discPrefix = [6011, 644, 645, 646, 647, 648, 649, 65];
+  var discLength = [16, 19];
+  //Maestro Info
+  var moPrefix = [5018, 5020, 5038, 6304];
+  var moLength = [12, 13, 14, 15, 16, 17, 18, 19];
+  //Switch Info
+  var switchPrefix = [4903, 4905, 4911, 4936, 564182, 633110, 6333, 6759];
+  var switchLength = [16, 18, 19];
+
+  if (parseInt(cnTwoPrefix) === dcPrefix[0] || parseInt(cnTwoPrefix) === dcPrefix[1] && cnLength === dcLength) {
     return "Diner's Club";
-  } else if (cnPrefix === axPrefix[0] || cnPrefix === axPrefix[1] && cnLength === axLength) {
+  } else if (parseInt(cnTwoPrefix) === axPrefix[0] || parseInt(cnTwoPrefix) === axPrefix[1] && cnLength === axLength) {
     return "American Express";
   }
+  for (var k = 0; k < switchPrefix.length; k++) {
+    var currPre = switchPrefix[k];
+    if ((parseInt(cnFourPrefix) === currPre || parseInt(cnSixPrefix) === currPre) && (cnLength === 16 || cnLength === 18 || cnLength === 19)) {
+      return 'Switch'
+    }5
+  }
+  if ((parseInt(cnOnePrefix) === visaPrefix) && (cnLength === 13 ||cnLength === 16 || cnLength === 19)) {
+    return "Visa";
+  }
+  if (mcPrefix.includes(parseInt(cnTwoPrefix)) && mcLength === cnLength) {
+    return "MasterCard";
+  }
+  for (var i = 0; i < discPrefix.length; i++) {
+    if (discPrefix[i] === parseInt(cnTwoPrefix)
+      || cnThreePrefix.includes(discPrefix[i])
+      || cnFourPrefix.includes(discPrefix[i])
+      && discLength.includes(cnLength)) {
+        return 'Discover';
+      }
+  }
+  for (var j = 0; j < moPrefix.length; j++) {
+    if (moPrefix[j] === parseInt(cnFourPrefix)
+      && moLength.includes(cnLength)) {
+        return 'Maestro';
+      }
+  }
+  if ((cnSixPrefix >= 622126 && cnSixPrefix <= 622925)
+  || (cnThreePrefix >= 624 && cnThreePrefix <= 626)
+  || (cnFourPrefix >= 6282 && cnFourPrefix <= 6288)
+  && cnLength >= 16 && cnLength <= 19) {
+    return 'China UnionPay';
+  }
+
+  // if (parseInt(cnFourPrefix) === 4903 || parseInt(cnFourPrefix) === 4905 || parseInt(cnFourPrefix) === 4911 || parseInt(cnFourPrefix) === 4936 || parseInt(cnFourPrefix) === 6333 || parseInt(cnFourPrefix) === 6759 && cnLength === 16 || cnLength === 18 || cnLength === 19) {
+  //   return 'Switch'
+  // }
 };
